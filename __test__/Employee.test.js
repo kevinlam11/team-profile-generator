06cd3f98bb35e1d349daa57b71990cc7b7ac9542
kevnviek303@yahoo.com <kevnivek303@yahoo.com>
2022-03-22 15:45:41 -0400

    updated JS

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,1 +1,28 @@
-const Employee = require("./lib/Employee");
+const { test, expect } = require("@jest/globals");
+const Employee = require("../lib/Employee");
+
+const testEmployee = new Employee("Bob", "fake id", "bob@bob.com");
+
+test("created an employee object", () => {
+  expect(typeof testEmployee).toBe("object");
+});
+
+test("can get and set employees name", () => {
+  expect(testEmployee.name).toBe("Bob");
+  expect(testEmployee.getName()).toBe("Bob");
+});
+
+test("can get and set employees id", () => {
+  expect(testEmployee.id).toBe("fake id");
+  expect(testEmployee.getId()).toBe("fake id");
+});
+
+test("can get and set employees email", () => {
+  expect(testEmployee.email).toBe("bob@bob.com");
+  expect(testEmployee.getEmail()).toBe("bob@bob.com");
+});
+
+test("can get employee role", () => {
+  expect(testEmployee.role).toBe("Employee");
+  expect(testEmployee.getRole()).toBe("Employee");
+});
